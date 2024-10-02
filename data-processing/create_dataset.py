f6125862c33c6fb6d3108aa3d30e2688f53da63e
Landon Key <>
2024-10-02 16:22:46 -0500

    Dataset enhancements

--- data-processing/create_dataset.py
+++ data-processing/create_dataset.py
@@ -6,11 +6,13 @@ from datasets import Dataset
 from util import load_models, apply_template
 from sliding_window import process_files as process_files_sw
 from llama_data_pre_processing import process_files as process_files_llama
+from llama_data_pre_processing import generate_story_data
+from llama_prompts import be_brief, only_return_prediction
 
 DEFAULT_PRE="Below I have provided a short history of minecraft game data and player actions, act as an expert minecraft player and suggest the next appropriate action to be taken next based on the game data provided.\n\n"
 
 functions = [
-                ("function: move",            5000),
+                ("function: move",            10000),
                 ("function: interact_block",  1000),
                 ("function: player_sleep",    1000),
                 ("function: use_item",        1000),
@@ -33,7 +35,12 @@ def create_dataset_llama(tokenizer, input_dir, output_dir, dataset_dir, model="l
 
     for f, max in functions:
         i = 0
-        for human, gpt in process_files_llama(model, input_dir, output_dir, f):        
+        for human, gpt, gpt_summary, action in process_files_llama(model, input_dir, output_dir, f):        
+            ##
+            ## It is important to note that all non-human responses here include the predection at the end
+            ## This is intended to provide better flexibility in steering the LLM to be more flexible in its 
+            ## response lengths for speed and performance.
+            ##
             data = [
                 {
                     "from": "human",
@@ -44,10 +51,66 @@ def create_dataset_llama(tokenizer, input_dir, output_dir, dataset_dir, model="l
                     "value": gpt.strip()
                 }
             ]
+            data_sm = [
+                {
+                    "from": "human",
+                    "value": be_brief() + " " + human.strip()
+                },
+                {
+                    "from": "gpt",
+                    "value": gpt_summary.strip()
+                }
+            ]
+            data_prediction = [
+                {
+                    "from": "human",
+                    "value": only_return_prediction() + " " + human.strip()
+                },
+                {
+                    "from": "gpt",
+                    "value": action.strip()
+                }
+            ]
             messages.append([data])
+            messages.append([data_sm])
+            messages.append([data_prediction])
             i += 1
             if i >= max:
                 break
+
+    print(f"Number of messages loaded: {len(messages)}")
+    for idx, message in enumerate(messages[:5]):
+        print(f"Message {idx}: Type={type(message)}, Content={message}")
+
+
+    print(f"Applying Template...")
+
+    dataset_list = apply_template(tokenizer, messages)
+    dataset = Dataset.from_list(dataset_list)
+
+    dataset.save_to_disk(dataset_dir)
+
+    print(f"Data saved to {dataset_dir}")
+
+    return dataset
+
+def create_dataset_story(tokenizer, output_dir, dataset_dir, model="llama3.1"):
+    messages = []
+
+    print(f"Processing Data via Ollama w/ {model}...")
+
+    for human, gpt in generate_story_data(model, output_dir):
+        data = [
+            {
+                "from": "human",
+                "value": human.strip()
+            },
+            {
+                "from": "gpt",
+                "value": gpt.strip()
+            }
+        ]
+        messages.append([data])
 
     print(f"Number of messages loaded: {len(messages)}")
     for idx, message in enumerate(messages[:5]):
@@ -155,6 +218,7 @@ def main():
     parser.add_argument('--sliding-window-size', type=int, default=3, help='Size of the sliding window.')
     parser.add_argument('--pre-prompt', type=str, default=None, help='Prefix to LLM training prompt.')
     parser.add_argument('--use-llama', action='store_true', help='Process data with llama via ollama')
+    parser.add_argument('--story-dataset', action='store_true', help='Process story data with llama via ollama')
     parser.add_argument('--dataset-name', type=str, default="dataset", help='Name of the dataset.')
 
     args = parser.parse_args()
@@ -166,11 +230,14 @@ def main():
     sliding_window_size = args.sliding_window_size
     pre = args.pre_prompt
     use_llama = args.use_llama
+    story_dataset = args.story_dataset
 
     model, tokenizer = load_models()
 
     if use_llama:
         create_dataset_llama(tokenizer, input_dir, './output/llama', dataset_dir)
+    elif story_dataset:
+        create_dataset_story(tokenizer, './output/llama', dataset_dir)
     elif pre == None:
         create_dataset(tokenizer, input_dir, dataset_dir, sliding_window_size)
     else:
--- data-processing/llama_data_pre_processing.py
+++ data-processing/llama_data_pre_processing.py
@@ -4,6 +4,8 @@ import argparse
 from util import extract_timestamp
 import subprocess
 import re
+import hashlib
+from llama_prompts import get_random_prompt, get_story_prompts, next_action_preprompt
 
 # Define the function to call Ollama
 def call_ollama(model, prompt):
@@ -26,6 +28,26 @@ def call_ollama(model, prompt):
         print(f"An error occurred: {e}")
         return "ERROR"
 
+def generate_story_data(model, output_dir):
+    prompts = get_story_prompts()
+    for prompt in prompts:
+        hash = hashlib.sha1(prompt.encode("UTF-8")).hexdigest()
+        file_path = os.path.join(output_dir, f"{hash}.txt")
+        
+        if os.path.exists(file_path):
+            print(f"File {file_path} already exists. Reading data.")
+            with open(file_path, "r") as f:
+                existing_story_data = f.read()
+                yield (prompt, existing_story_data)
+        else:
+            print(f"Generating story data for `{prompt}`")
+            response = call_ollama(model, prompt)
+            with open(file_path, "w") as f:
+                f.write(response)
+            print(f"Saved to {file_path}.")
+            yield (prompt, response)
+
+
 def get_action_yaml(text):
     # Use regex to find the last occurrence of "action:" at the start of a line
     match = list(re.finditer(r'^action:', text, re.MULTILINE))
@@ -36,7 +58,7 @@ def get_action_yaml(text):
         human_value = text[:last_action_index]
         gpt_value = text[last_action_index:]
         
-        return f"\n\nGiven my analysis above the next predicted action would be:\n\n```yml\n{gpt_value}\n```"
+        return f"\n\{next_action_preprompt()}:\n\n```yml\n{gpt_value}\n```"
     else:
         raise Exception("action section not found")
 
@@ -64,7 +86,28 @@ def get_files_with_keyword(input_dir, keyword):
         print(f"An error occurred during grep execution: {e}")
         return []
 
+def get_summary(model, base_filename, output_dir, full_text):
+    output_filename = f"{base_filename}-llama-sm.txt"
+    output_path = os.path.join(output_dir, output_filename)
 
+    if os.path.exists(output_path):
+        print(f"Output file {output_filename} already exists. Skipping...")
+        with open(output_path, 'r') as output_file:
+            return output_file.read()
+    else:
+        response = "summary"
+        while "summary" in response.lower() or "250 words" in response.lower():
+            response = call_ollama(
+                model, 
+                "I'm a software engineer using large language models for summarization. Summarize the following text in under 250 words while maintaining the perspective of an minecraft expert analyst. Never mention that you are summarizing only provide the sumarized analysis:\n\n" + full_text
+            )
+                
+        # Save the response to the output file
+        with open(output_path, 'w') as output_file:
+            output_file.write(response)
+            print(f"Response saved to {output_path}")
+
+        return response
 
 def process_files(model, input_dir, output_dir, keyword=None):    
     # Get all the .yaml files in the input directory
@@ -92,21 +135,27 @@ def process_files(model, input_dir, output_dir, keyword=None):
             with open(next_file_path, 'r') as next_file:
                 data = file.read()
                 next_data = next_file.read()
+                action_yaml = get_action_yaml(next_data)
                 # Create output file name based on input file name
                 base_filename = os.path.splitext(filename)[0]
                 output_filename = f"{base_filename}-llama.txt"
                 output_path = os.path.join(output_dir, output_filename)
 
-                prompt = f"Act as an expert Minecraft player who can understand a player's actions by viewing the game state at the time the action was given. I will provide you with the game state and the action taken in YAML form, and you will tell me why a player may have taken the given action using your knowledge of Minecraft and the game state, be highly detailed and provide your reasoning step by step for the sample data below:\n\n{data}\n\n"
+                prompt = get_random_prompt() + f"{data}"
 
                 if os.path.exists(output_path):
                     print(f"Output file {output_filename} already exists. Skipping...")
                     with open(output_path, 'r') as output_file:
                         existing_response = output_file.read()
-                        yield (prompt, existing_response + get_action_yaml(next_data))
+                        yield (
+                                prompt, 
+                                existing_response + action_yaml,
+                                get_summary(model, base_filename, output_dir, existing_response) + action_yaml,
+                                action_yaml
+                            )
+
                     continue
-                
-                
+
                 response = call_ollama(model, prompt)
                 
                 # Save the response to the output file
@@ -114,7 +163,12 @@ def process_files(model, input_dir, output_dir, keyword=None):
                     output_file.write(response)
                     print(f"Response saved to {output_path}")
 
-                yield (prompt, response + get_action_yaml(next_data))
+                yield (
+                        prompt, 
+                        response + action_yaml,
+                        get_summary(model, base_filename, output_dir, response) + action_yaml,
+                        action_yaml
+                    )
 
 def main():
     parser = argparse.ArgumentParser(description='LLM training')
